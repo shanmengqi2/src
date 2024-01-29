118a01b2a4ef8b5eb4d9ca0a658f156ceba82694
shanmengqi <shanmengqi@163.com>
2024-01-29 23:55:12 +0800

    footer&header

---
+++ Yumeki_Tasks/ex2.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500&display=swap" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Source+Serif+Pro:400,600&display=swap" rel="stylesheet">
+
+    <link rel="stylesheet" href="footer/fonts/icomoon/style.css">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="footer/css/bootstrap.min.css">
+
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="footer/css/style.css">
+</head>
+<body>
+<?php include 'header.php'?>
+<div class="content d-flex align-items-center bg-light"><h3 class="w-100 text-center">EX2:HEADER&FOOTER</h3></div>
+<?php include 'footer/footer.php'?>
+</body>
+</html>
+
